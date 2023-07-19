315f2f634afc96645f59c60099706bd572690113
Bex Edmondson <bexedmondson@gmail.com>
2023-07-19 01:58:22 +0100

    removing old stuff

--- pages/multiselectcomponent.js
+++ pages/multiselectcomponent.js
@@ -2,12 +2,6 @@ import React, { useState } from 'react';
 import Select from 'react-select';
 import london from '../data/london.json';
 
-const options = [
-  { value: 'chocolate', label: 'Chocolate' },
-  { value: 'strawberry', label: 'Strawberry' },
-  { value: 'vanilla', label: 'Vanilla' },
-];
-
 const selectedOptions = [];
 
 
