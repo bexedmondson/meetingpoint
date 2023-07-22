dc390d163dd99508f4a1acc7d26471d48f131d4a
Bex Edmondson <bexedmondson@gmail.com>
2023-07-22 13:51:17 +0100

    map building

--- src/findMeetingPoint.js
+++ src/findMeetingPoint.js
@@ -15,24 +15,38 @@ function getStationFromId(id) {
     }).station;
 }
 
-function buildMap()
-{
+function buildMap() {
     london.connections.forEach((c) => {
-        
+        let connections1 = {};
+        let connections2 = {};
+        if (c.station1 in map) {
+            connections1 = map[c.station1];
+        }
+        if (c,station2 in map) {
+            connections2 = map[c.station2];
+        }
+
+        connections1[c.station2] = c.time;
+        connections2[c.station1] = c.time;
+
+        map[c.station1] = connections1;
+        map[c.station2] = connections2;
     });
+
+
 }
 
 
 function findAveragePathLength(starts, end) {
     var totalPath = 0;
 
-    (console.log(london.connections), '')
+    (console.log(map), '')
 
     starts.forEach(start => {
         let startId = getStationId(start);
         let endId = getStationId(end);
 
-        let thisPath = find_path(london.connections, startId, endId);
+        let thisPath = find_path(map, startId, endId);
         
         (console.log(thisPath), '')
         totalPath += thisPath.length;
