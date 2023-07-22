c616036767e34724ddb642ed58eea95d0ece99f9
Bex Edmondson <bexedmondson@gmail.com>
2023-07-22 16:32:15 +0100

    trying to get pathfinding to actually work right

--- src/MeetingPoint.svelte
+++ src/MeetingPoint.svelte
@@ -1,23 +1,31 @@
 <script>
-    import { findMeetingPoint } from './findMeetingPoint.js'
+    import { buildMap, findMeetingPoint, getStationFromId } from './findMeetingPoint.js'
 
-    let hasResult = false
-    let meetingPoint = null
+    var hasResult = false
+    var meetingPointInfo = null
 
     function onclick() {
+        meetingPointInfo = findMeetingPoint(starts, ends)
         hasResult = true;
-        meetingPoint = findMeetingPoint(starts, ends)
+
+        (console.log(meetingPointInfo), '')
     };
     
     export let starts = []
     export let ends = []
 </script>
 
-
-<button on:click={onclick}><h3>  Find  </h3></button>
-{#if hasResult}
-    <h2>{meetingPoint}</h2>
-{/if}
+{#await buildMap()}
+    <p>Loading...</p>
+{:then map}
+    <button on:click={onclick}><h3>  Find  </h3></button>
+    {#if hasResult}
+        <h2>{meetingPointInfo.end}</h2>
+        {#each meetingPointInfo.path as pathPoint}
+            <h3>{getStationFromId(pathPoint)}</h3>
+        {/each}
+    {/if}
+{/await}
 
 <style>
     button {
--- src/findMeetingPoint.js
+++ src/findMeetingPoint.js
@@ -3,6 +3,12 @@ import london from '../data/london.json';
 
 var map = [];
 
+function PathInfo(average, end, path) {
+    this.average = average;
+    this.end = end;
+    this.path = path;
+}
+
 function getStationId(station) {
     return london.stations.find((s) => {
         return s.name == station;
@@ -11,43 +17,49 @@ function getStationId(station) {
 
 function getStationFromId(id) {
     return london.stations.find((s) => {
-        return s.id == id;
-    }).station;
+        return s.id === id;
+    }).name;
 }
 
-function buildMap() {
+async function buildMap() {
     london.connections.forEach((c) => {
         let connections1 = {};
         let connections2 = {};
+
         if (c.station1 in map) {
             connections1 = map[c.station1];
         }
-        if (c,station2 in map) {
+        if (c.station2 in map) {
             connections2 = map[c.station2];
         }
 
-        connections1[c.station2] = c.time;
+        connections1[c.station2] = c.time; 
         connections2[c.station1] = c.time;
 
         map[c.station1] = connections1;
         map[c.station2] = connections2;
     });
 
-
+    return map;
 }
 
 
 function findAveragePathLength(starts, end) {
     var totalPath = 0;
 
-    (console.log(map), '')
+    var endPathInfo = new PathInfo();
+    endPathInfo.end = end;
 
     starts.forEach(start => {
         let startId = getStationId(start);
         let endId = getStationId(end);
 
+        (console.log(startId + ' ' + endId), '')
+
         let thisPath = find_path(map, startId, endId);
-        
+
+        endPathInfo["path"] = thisPath;
+
         (console.log(thisPath), '')
         totalPath += thisPath.length;
     });
@@ -57,26 +69,26 @@ function findAveragePathLength(starts, end) {
     let average = totalPath / starts.length;
 
     (console.log(average), '')
+
+    endPathInfo.average = average;
     
-    return average;
+    return endPathInfo;
 }
 
 function findMeetingPoint(starts, ends) {
-    var minEnd = ends[0];
-    var minAverage = -1;
-
-    map = buildMap();
+    var minPathInfo = {average: -1};
 
     ends.forEach(end => {
-        let averagePathLength = findAveragePathLength(starts, end);
-        if (minAverage === -1 || minAverage > averagePathLength)
+        let endPathInfo = findAveragePathLength(starts, end);
+        if (minPathInfo.average === -1 || minPathInfo.average > endPathInfo.average)
         {
-            minAverage = averagePathLength;
-            minEnd = end;
+            minPathInfo = endPathInfo;
         }
     });
 
-    return minEnd;
+    (console.log(minPathInfo), '')
+
+    return minPathInfo;
 }
 
-export {findMeetingPoint};
+export {buildMap, findMeetingPoint, getStationFromId};
