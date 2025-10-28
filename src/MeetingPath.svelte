0ede95c65bc60960d2f990e73a862ac7b341b666
Bex Edmondson <bexedmondson@gmail.com>
2025-10-28 16:58:03 +0000

    adding components to manage displaying each path's info

---
+++ src/MeetingPath.svelte
@@ -0,0 +1,14 @@
+<script>
+    import PathFromTo from "./PathFromTo.svelte";
+
+    export let meetingPathInfo;
+</script>
+
+{#if meetingPathInfo === undefined || meetingPathInfo.finalPath === undefined}
+    <p>No path found <PathFromTo path={meetingPathInfo.finalPath} /> :(</p>
+{:else}
+    <p>Path <PathFromTo path={meetingPathInfo.finalPath} />, duration {meetingPathInfo.cost}</p>
+{/if}
+
+<style>
+</style>
--- src/MeetingPoint.svelte
+++ src/MeetingPoint.svelte
@@ -1,5 +1,6 @@
 <script>
     import { buildMap, findMeetingPoint, getStationFromId } from './findMeetingPoint.js'
+    import MeetingPath from "./MeetingPath.svelte";
 
     var meetingPointInfo = null
 
@@ -14,13 +15,18 @@
 {#await buildMap()}
     <p>Loading...</p>
 {:then map}
-    <button on:click={onclick}><h3>  Find  </h3></button>
+    <button on:click={onclick}><b>  Find  </b></button>
 
     {#if meetingPointInfo !== null}
         {#if meetingPointInfo === undefined || meetingPointInfo.end === undefined}
             <h2>No path found :(</h2>
         {:else}
             <h3>Best option: {meetingPointInfo.end}, average time: {meetingPointInfo.average}</h3>
+
+            {#each meetingPointInfo.paths as path}
+                <MeetingPath meetingPathInfo={path} />
+            {/each}
+
         {/if}
     {/if}
 {/await}
@@ -29,6 +35,6 @@
     button {
         min-height: 3em;
         min-width: 5em;
-        padding: 0em;
+        padding: 0;
     }
 </style>
