c7a0e19d8bc40db0693c37243e2340a8fcedbb3c
Bex Edmondson <bexedmondson@gmail.com>
2025-11-05 17:49:11 +0000

    small refactor

---
+++ src/PathSeparator.svelte
@@ -0,0 +1,18 @@
+<script>
+</script>
+
+<div style="position: relative">
+    <svg class="pathSeparator" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke="#111" stroke-width="8" stroke-linecap="round" stroke-linejoin="round"><path d="m7 6 5 5 5-5"/><path d="m7 13 5 5 5-5"/></svg>
+    <svg class="pathSeparator" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke="#eee" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"><path d="m7 6 5 5 5-5"/><path d="m7 13 5 5 5-5"/></svg>
+    <!--ChevronsDown class="pathSeparator" strokeWidth="8" color="#111" />
+    <ChevronsDown class="pathSeparator" strokeWidth="4" /-->
+</div>
+
+<style>
+.pathSeparator {
+    position: absolute;
+    transform: translate(-50%, -95%);
+    width: 36px;
+    height: 36px;
+}
+</style>
